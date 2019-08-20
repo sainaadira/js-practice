67397e5e2019206dab03b9600b357288ebc97821
sainaadira <snaadira@gmail.com>
2019-08-20 08:53:02 -0400

    updated code

---
+++ practice-functions.js
@@ -0,0 +1,56 @@
+
+// course follow along
+
+function input (argument), code, output (return value)
+
+let greetUser = function (){
+    console.log('Welcome user!')
+}
+
+greetUser()
+
+
+let square= function (num){
+    let result = num *num
+}
+
+let value = square (3)
+let otherValue = square(10)
+
+console.log(value)
+console.log (otherValue)
+
+
+// Challenge Area
+
+function convertFarToCel(fahrenheit){
+    let celius = (fahrenheit - 32) * 5 / 9 
+    return celius
+}
+
+let tempOne = convertFarToCel(32)
+let tempTwo = convertFarToCel(68)
+
+console.log(tempOne)
+console.log (tempTwo)
+
+
+// My Functions 
+let converFtToIn = function (foot) {
+    let feet = (foot * 12)
+    return feet
+}
+
+let footOne = converFtToIn(10)
+let footTwo = converFtToIn(3)
+
+console.log(footOne)
+console.log(footTwo)
+
+// __________________________________
+
+let convertKiloToMi = function (){
+
+
+}
+
