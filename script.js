54c6aabb082d80920c11de00308138503fcc3ebd
sainaadira <snaadira@gmail.com>
2019-08-20 22:15:49 -0400

    completed problem

--- script.js
+++ script.js
@@ -19,32 +19,32 @@ const obj={
 // ___________________________________________
 // create an immediately invoked function expression to do the following:
 
-// (()=>{
-//     const newArray=[]
-//     let sumArray = []
-//     let numOfSplits; 
-//     // pushed the number and string variable into a new array 
-//     arr2.push(num1,num2,str1.length)
-//     // created a formula here to get the number of split (3) and the conditional here is stating if the 
-//     // number array is divisible by three  
-//     if(arr2.length % 3 ===0){
-//         numOfSplits = 3; 
-//     }
-//     // the for loop here is itierating through the array with i being 0 and  i is greater than or equal to the the number of splits +1
-//     // and the plus i represents the incremented iteration
-//     for(i=0; i <= numOfSplits + 1; i += arr2.length/numOfSplits){
-//         newArray.push(arr2.slice(i, arr2.length/numOfSplits + i));
-//     }
-//      // in this for each loop passing in arr to the newArray means that i am accessing 
-//     // the array within the newArray
-//     newArray.forEach((arr)=>{
-//         // here i am accessing the indices of arr and adding them together 
-//        arr[0] + arr[1]
-//         sumArray.push([arr[0] + arr[1], arr])
-//     });
-//         console.log(sumArray);
+(()=>{
+    const newArray=[]
+    let sumArray = []
+    let numOfSplits; 
+    // pushed the number and string variable into a new array 
+    arr2.push(num1,num2,str1.length)
+    // created a formula here to get the number of split (3) and the conditional here is stating if the 
+    // number array is divisible by three  
+    if(arr2.length % 3 ===0){
+        numOfSplits = 3; 
+    }
+    // the for loop here is itierating through the array with i being 0 and  i is greater than or equal to the the number of splits +1
+    // and the plus i represents the incremented iteration
+    for(i=0; i <= numOfSplits + 1; i += arr2.length/numOfSplits){
+        newArray.push(arr2.slice(i, arr2.length/numOfSplits + i));
+    }
+     // in this for each loop passing in arr to the newArray means that i am accessing 
+    // the array within the newArray
+    newArray.forEach((arr)=>{
+        // here i am accessing the indices of arr and adding them together 
+       arr[0] + arr[1]
+        sumArray.push([arr[0] + arr[1], arr])
+    });
+        console.log(sumArray);
         
-// })();
+})();
 
 
 
